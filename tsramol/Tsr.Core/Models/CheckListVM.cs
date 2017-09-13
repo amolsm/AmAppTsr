0b0b753a2c00c6d1f1e059ec4e14c8812a1a9111
santosh <santosh.y@24hourdesignstudio.net>
2017-09-13 14:45:19 +0530

    13SepSantosh245
    commit

---
+++ tsramol/Tsr.Core/Models/CheckListVM.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tsr.Core.Models
+{
+    public class CheckListVM
+    {
+        public int ApplicationId { get; set; }
+        public string Course { get; set; }
+        public DateTime? Batchfrom { get; set; }
+        public DateTime? Batchto { get; set; }
+        public int SrNo { get; set; }
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime? DOB { get; set; }
+        public string Cdcno { get; set; }
+        public string PassportNo { get; set; }
+        public string Rank { get; set; }
+        public string Grade { get; set; }
+        public string IndosNo { get; set; }
+        public string Sign { get; set; }
+
+    }
+}
--- tsramol/Tsr.Web/Controllers/CertificationController.cs
+++ tsramol/Tsr.Web/Controllers/CertificationController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.IO;
@@ -21,7 +23,341 @@ namespace Tsr.Web.Controllers
         #region CheckList
         public ActionResult CheckList()
         {
-            return View();
+            ViewBag.Categories = new SelectList(db.CourseCategories.ToList(), "CourseCategoryId", "CategoryName");
+            var obj = new List<CheckListVM>();
+            return View(obj);
+        }
+
+        [HttpGet]
+        public ActionResult GetCheckList(int? BatchId)
+        {
+            var list = (from ap in db.Applications.AsEnumerable()
+                        join b in db.Batches on ap.BatchId equals b.BatchId
+                        join fr in db.FeeReceipts on ap.ApplicationId equals fr.ApplicationId
+                        join sd in db.StudentFeeDetails on ap.ApplicationId equals sd.ApplicationId
+                        join cr in db.Courses on ap.CourseId equals cr.CourseId
+                        join op in db.OnlinePaymentInfos on ap.ApplicationId equals op.ApplicationId
+                        where (b.BatchId == BatchId)
+                        select new CheckListVM
+                        {
+                            Course = cr.CourseName,
+                            Batchfrom = b.StartDate,
+                            Batchto = b.EndDate,
+                            StudentId = ap.ApplicationId,
+                            Name = ap.FirstName + " " + ap.LastName,
+                            DOB = ap.DateOfBirth,
+                            Cdcno = ap.CdcNo,
+                            PassportNo = ap.PassportNo,
+                            Rank = ap.RankOfCandidate,
+                            Grade = ap.GradeOfCompetencyNo,
+                            IndosNo = ap.InDosNo
+
+
+                        }).ToList();
+            return PartialView("_ApplicantsList", list.ToList());
+        }
+
+        public ActionResult Export(int id)
+        {
+            using (MemoryStream stream = new System.IO.MemoryStream())
+            {
+                int BatchIds = 0;
+                BatchIds = id;
+                string coursename = string.Empty;
+                string StartDate = string.Empty;
+                string EndDate = string.Empty;
+                string BatchCode = string.Empty;
+                var batch = from b in db.Batches
+                            join c in db.Courses on b.CourseId equals c.CourseId
+                            where b.BatchId == id
+                            select new { c.CourseName, b.StartDate, b.EndDate, b.BatchCode };
+                var list = (from ap in db.Applications.AsEnumerable()
+                            join b in db.Batches on ap.BatchId equals b.BatchId
+                            join fr in db.FeeReceipts on ap.ApplicationId equals fr.ApplicationId
+                            join sd in db.StudentFeeDetails on ap.ApplicationId equals sd.ApplicationId
+                            join cr in db.Courses on ap.CourseId equals cr.CourseId
+                            join op in db.OnlinePaymentInfos on ap.ApplicationId equals op.ApplicationId
+                            where (b.BatchId == id)
+                            select new CheckListVM
+                            {
+                                Course = cr.CourseName,
+                                Batchfrom = b.StartDate,
+                                Batchto = b.EndDate,
+                                StudentId = ap.ApplicationId,
+                                Name = ap.FirstName + " " + ap.LastName,
+                                DOB = ap.DateOfBirth,
+                                Cdcno = ap.CdcNo,
+                                PassportNo = ap.PassportNo,
+                                Rank = ap.RankOfCandidate,
+                                Grade = ap.GradeOfCompetencyNo,
+                                IndosNo = ap.InDosNo
+                            });
+                foreach (var s in batch)
+                {
+                    BatchCode = s.BatchCode;
+                    coursename = s.CourseName;
+                    StartDate = Convert.ToDateTime(s.StartDate).ToString("dd-MM-yyyy");
+                    EndDate = Convert.ToDateTime(s.EndDate).ToString("dd-MM-yyyy");
+
+                }
+                iTextSharp.text.Document pdfDoc = new iTextSharp.text.Document(PageSize.A4.Rotate());
+                PdfWriter writer = PdfWriter.GetInstance(pdfDoc, stream);
+                pdfDoc.Open();
+                pdfDoc.AddTitle("Check List");
+
+                PdfPTable table = new PdfPTable(16);
+                iTextSharp.text.Font f = FontFactory.GetFont("Arial", 10, iTextSharp.text.Font.BOLD, BaseColor.BLACK);
+                FontFactory.Register("C:\\Windows\\Fonts\\OLDENGL.ttf", "OldEnglishTextMT");
+                iTextSharp.text.Font O = FontFactory.GetFont("OldEnglishTextMT", 25, iTextSharp.text.Font.BOLD, BaseColor.BLACK);
+                table.TotalWidth = 775f;
+                table.LockedWidth = true;
+                Paragraph comb = new Paragraph();
+                comb.Add(new Chunk("SIR MOHAMMAD YUSUF SEAMEN WELFARE FOUNDATION\n Tel: 022-27212800/900   Fax: 02227212495\n\n ", f));
+                comb.Add(new Chunk("Training Ship Rahaman\n", O));
+                comb.Add(new Chunk("\n CHECK LIST for course :  " + (coursename == null ? "" : coursename) + "   Batch from  " + (StartDate == null ? "" : StartDate.ToString()) + "  to  " + (EndDate == null ? "" : EndDate.ToString()) + "  (" + BatchCode + ")", f));
+
+                PdfPCell header = new PdfPCell(new Phrase(comb));
+                header.Colspan = 16;
+                header.HorizontalAlignment = Element.ALIGN_CENTER;
+                header.UseVariableBorders = true;
+                header.BackgroundColor = BaseColor.WHITE;
+                table.AddCell(header);
+                Chunk main = new Chunk("Check List", f);
+                PdfPCell mainheading = new PdfPCell(new Phrase(main));
+                mainheading.BackgroundColor = BaseColor.WHITE;
+                mainheading.HorizontalAlignment = Element.ALIGN_CENTER;
+                mainheading.Colspan = 16;
+                //table.AddCell(mainheading);
+
+                Chunk cthead1 = new Chunk("SrNo", f);
+                PdfPCell thead1 = new PdfPCell(new Phrase(cthead1));
+                thead1.Border = Rectangle.LEFT_BORDER;
+                thead1.BackgroundColor = BaseColor.WHITE;
+                table.AddCell(thead1);
+                Chunk cthead2 = new Chunk("Student Id", f);
+                PdfPCell thead2 = new PdfPCell(new Phrase(cthead2));
+                thead2.Border = Rectangle.NO_BORDER;
+                thead2.BackgroundColor = BaseColor.WHITE;
+                thead2.Colspan = 2;
+                table.AddCell(thead2);
+                Chunk cthead3 = new Chunk("Name", f);
+                PdfPCell thead3 = new PdfPCell(new Phrase(cthead3));
+                thead3.Border = Rectangle.NO_BORDER;
+                thead3.Colspan = 2;
+                thead3.BackgroundColor = BaseColor.WHITE;
+                table.AddCell(thead3);
+                Chunk cthead4 = new Chunk("DOB", f);
+                PdfPCell thead4 = new PdfPCell(new Phrase(cthead4));
+                thead4.Border = Rectangle.NO_BORDER;
+                thead4.BackgroundColor = BaseColor.WHITE;
+                thead4.Colspan = 2;
+                table.AddCell(thead4);
+                Chunk cthead5 = new Chunk("Cdcno", f);
+                PdfPCell thead5 = new PdfPCell(new Phrase(cthead5));
+                thead5.Border = Rectangle.NO_BORDER;
+                thead5.BackgroundColor = BaseColor.WHITE;
+                thead5.Colspan = 2;
+                table.AddCell(thead5);
+                Chunk cthead6 = new Chunk("Passport No", f);
+                PdfPCell thead6 = new PdfPCell(new Phrase(cthead6));
+                thead6.Border = Rectangle.NO_BORDER;
+                thead6.Colspan = 2;
+                thead6.BackgroundColor = BaseColor.WHITE;
+                table.AddCell(thead6);
+                Chunk cthead7 = new Chunk("Rank", f);
+                PdfPCell thead7 = new PdfPCell(new Phrase(cthead7));
+                thead7.Border = Rectangle.NO_BORDER;
+                thead7.BackgroundColor = BaseColor.WHITE;
+                table.AddCell(thead7);
+                Chunk cthead8 = new Chunk("Grade", f);
+                PdfPCell thead8 = new PdfPCell(new Phrase(cthead8));
+                thead8.Border = Rectangle.NO_BORDER;
+                thead8.BackgroundColor = BaseColor.WHITE;
+                table.AddCell(thead8);
+                Chunk cthead9 = new Chunk("Indos No", f);
+                PdfPCell thead9 = new PdfPCell(new Phrase(cthead9));
+                thead9.Border = Rectangle.NO_BORDER;
+                thead9.BackgroundColor = BaseColor.WHITE;
+                thead9.Colspan = 2;
+                table.AddCell(thead9);
+                Chunk cthead10 = new Chunk("Sign", f);
+                PdfPCell thead10 = new PdfPCell(new Phrase(cthead10));
+                thead10.Border = Rectangle.RIGHT_BORDER;
+                thead10.BackgroundColor = BaseColor.WHITE;
+                table.AddCell(thead10);
+
+
+                //actual width of table in points
+
+                //fix the absolute width of the table
+                table.LockedWidth = true;
+
+                //relative col widths in proportions - 1/3 and 2/3
+                float[] widths = new float[] { 100f, 100f, 100f, 100f, 100f, 100f, 100f, 100f, 100f, 100f, 100f, 100f, 100f, 100f, 100f, 100f };
+                table.SetWidths(widths);
+                table.HorizontalAlignment = 0;
+
+
+                //leave a gap before and after the table
+                table.SpacingBefore = 20f;
+                table.SpacingAfter = 20f;
+
+
+                int i = 0;
+                foreach (var s in list)
+                {
+                    i++;
+                    var dob = Convert.ToDateTime(s.DOB).ToString("dd-MM-yyyy");
+
+                    PdfPCell cell1 = new PdfPCell(new Phrase(i.ToString()));
+                    cell1.Border = Rectangle.LEFT_BORDER | Rectangle.BOTTOM_BORDER;
+                    table.AddCell(cell1);
+                    PdfPCell cell2 = new PdfPCell(new Phrase(s.StudentId.ToString()));
+                    cell2.Border = Rectangle.BOTTOM_BORDER;
+                    cell2.Colspan = 2;
+                    table.AddCell(cell2);
+                    PdfPCell cell3 = new PdfPCell(new Phrase(s.Name.ToString()));
+                    cell3.Border = Rectangle.BOTTOM_BORDER;
+                    cell3.Colspan = 2;
+                    table.AddCell(cell3);
+                    PdfPCell cell4 = new PdfPCell(new Phrase(dob.ToString()));
+                    cell4.Border = Rectangle.BOTTOM_BORDER;
+                    cell4.Colspan = 2;
+                    table.AddCell(cell4);
+                    PdfPCell cell5 = new PdfPCell(new Phrase(s.Cdcno.ToString()));
+                    cell5.Border = Rectangle.BOTTOM_BORDER;
+                    cell5.Colspan = 2;
+                    table.AddCell(cell5);
+                    PdfPCell cell6 = new PdfPCell(new Phrase(s.PassportNo==null?"":s.PassportNo.ToString()));
+                    cell6.Border = Rectangle.BOTTOM_BORDER;
+                    cell6.Colspan = 2;
+                    table.AddCell(cell6);
+                    PdfPCell cell7 = new PdfPCell(new Phrase(""));
+                    cell7.Border = Rectangle.BOTTOM_BORDER;
+                    table.AddCell(cell7);
+                    PdfPCell cell8 = new PdfPCell(new Phrase(""));
+                    cell8.Border = Rectangle.BOTTOM_BORDER;
+                    table.AddCell(cell8);
+                    PdfPCell cell9 = new PdfPCell(new Phrase(s.IndosNo == null ? "" : s.IndosNo.ToString()));
+                    cell9.Border = Rectangle.BOTTOM_BORDER;
+                    cell9.Colspan = 2;
+                    table.AddCell(cell9);
+                    PdfPCell cell10 = new PdfPCell(new Phrase(""));
+                    cell10.Border = Rectangle.RIGHT_BORDER | Rectangle.BOTTOM_BORDER;
+                    cell10.FixedHeight = 50f;
+                    table.AddCell(cell10);
+
+                }
+
+                Paragraph p = new Paragraph();
+                p.IndentationLeft = 0;
+                table.HorizontalAlignment = Element.ALIGN_LEFT;
+                p.Add(table);
+                pdfDoc.Add(p);
+                pdfDoc.Close();
+                return File(stream.ToArray(), "application/pdf");
+
+            }
+        }
+        public ActionResult CheckListEdit()
+        {
+            ViewBag.Categories = new SelectList(db.CourseCategories.ToList(), "CourseCategoryId", "CategoryName");
+            var obj = new List<CheckListVM>();
+            return View(obj);
+        }
+        [HttpGet]
+        public ActionResult GetCheckListEdit(int? BatchId)
+        {
+            var list = (from ap in db.Applications.AsEnumerable()
+                        join b in db.Batches on ap.BatchId equals b.BatchId
+                        join fr in db.FeeReceipts on ap.ApplicationId equals fr.ApplicationId
+                        join sd in db.StudentFeeDetails on ap.ApplicationId equals sd.ApplicationId
+                        join cr in db.Courses on ap.CourseId equals cr.CourseId
+                        join op in db.OnlinePaymentInfos on ap.ApplicationId equals op.ApplicationId
+                        where (b.BatchId == BatchId)
+                        select new CheckListVM
+                        {
+                            ApplicationId = ap.ApplicationId,
+                            Course = cr.CourseName,
+                            Batchfrom = b.StartDate,
+                            Batchto = b.EndDate,
+                            StudentId = ap.ApplicationId,
+                            Name = ap.FirstName + " " + ap.LastName,
+                            DOB = ap.DateOfBirth,
+                            Cdcno = ap.CdcNo,
+                            PassportNo = ap.PassportNo,
+                            Rank = ap.RankOfCandidate,
+                            Grade = ap.GradeOfCompetencyNo,
+                            IndosNo = ap.InDosNo
+
+
+                        }).ToList();
+            return PartialView("_ApplicantsListEdit", list.ToList());
+        }
+        [HttpGet]
+        public async Task<ActionResult> CheckListEditModal(int? id)
+        {
+            Application ap = await db.Applications.FindAsync(id);
+           
+            var obj = new CheckListVM
+                        {
+                            ApplicationId = ap.ApplicationId,
+                            FirstName = ap.FirstName,
+                            MiddleName=ap.MiddleName,
+                            LastName=ap.LastName,
+                            DOB = ap.DateOfBirth,
+                            Cdcno = ap.CdcNo,
+                            PassportNo = ap.PassportNo,
+                            Rank = ap.RankOfCandidate,
+                            Grade = ap.GradeOfCompetencyNo,
+                            IndosNo = ap.InDosNo
+
+
+                        };
+            
+            return PartialView("CheckListEditModal", obj);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CheckListEditModal(CheckListVM obj)
+        {
+            if (ModelState.IsValid)
+            {
+               
+                //var c = db.Applications.Where(m => m.ApplicationId == obj.ApplicationId).FirstOrDefault();
+                Application b = new Application
+                {
+                    ApplicationId=obj.ApplicationId,
+                    //ApplicationCode=c.ApplicationCode,
+                    //CategoryId=c.CategoryId,
+                    //CourseId=c.CourseId,
+                    //BatchId=c.BatchId,
+                    FirstName=obj.FirstName,
+                    MiddleName=obj.MiddleName,
+                    LastName=obj.LastName,
+                    //MotherName=c.MotherName,
+                    DateOfBirth = obj.DOB,
+                    //PlaceOfBirth=c.PlaceOfBirth,
+                    //Gender=c.Gender,
+                    //Citizenship=c.Citizenship,
+                    //Caste=c.Caste,
+                    CdcNo = obj.Cdcno,
+                    PassportNo = obj.PassportNo,
+                    RankOfCandidate = obj.Rank,
+                    GradeOfCompetencyNo = obj.Grade,
+                    InDosNo = obj.IndosNo
+
+                };
+               
+                //db.Entry(b).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return Json(new { success = true });
+            }
+
+
+            return PartialView("CheckListEditModal", obj);
         }
         #endregion
 
